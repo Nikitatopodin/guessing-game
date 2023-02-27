7dae0ef4df11f55f9377c108d2f371fc50eb4559
Nikitatopodin <Nikitatop1@gmail.com>
2023-02-27 23:53:08 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.guessedValue = this.minValue + Math.round((this.maxValue - this.minValue) / 2);
+        return this.guessedValue;
     }
 
     lower() {
-
+        this.maxValue = this.guessedValue;
     }
 
     greater() {
-
+        this.minValue = this.guessedValue
     }
 }
 
